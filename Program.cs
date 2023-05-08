151898c916bc2432777a187433150634378b71f4
GABRIEL SANT ANA DA SILVA <gabriel.sasilva4@senacsp.edu.br>
2023-05-08 16:56:44 -0300

    Atualização dos fundamentos

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+
+using CursoCSharp.Fundamentos;
 
 namespace CursoCSharp {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Variaveis e constantes - Fundamentos",VariaveisEConstantes.Executar },
+                {"Notação Ponto - Fundamentos",NotacaoPonto.Executar }
+            });
 
 
+            central.SelecionarEExecutar();
         }
     }
 }
- 
